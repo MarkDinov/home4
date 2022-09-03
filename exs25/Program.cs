bf45a83965472001cff2341a281afa7a07701a1e
Mark <mutofifin@mail.ru>
2022-09-03 18:39:43 +0300

    25 задание

---
+++ exs25/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ответ: " + Math.Pow(A, B));
