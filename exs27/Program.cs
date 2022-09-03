1cf23d1c22657e7b651225b6af10c895775d3ab7
Mark <mutofifin@mail.ru>
2022-09-03 18:40:11 +0300

    27 задание

---
+++ exs27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Summ(string a){
+	int summ = 0;
+	for(int i = 0; i < a.Length; i++){
+		int num = Convert.ToInt32(a[i].ToString());
+		summ += num;
+	}
+	return summ;
+}
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+Console.WriteLine("Сумма цифр числа равна: " + Summ(number));
